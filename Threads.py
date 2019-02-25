00cc7bbbed7281e4f356506a79590cbd70ce4c75
WenjieLuo2333 <43015006+WenjieLuo2333@users.noreply.github.com>
2019-02-24 22:33:59 -0500

    Add files via upload

--- Threads.py
+++ Threads.py
@@ -10,7 +10,7 @@ class get_in(threading.Thread):
 		self.o2 = o2
 	def run(self):
 		print("Input Thread Start")
-		path = input()
+		path = 'file.txt'
 		try:
 			with open(path,'r') as f:
 				data = f.readline().strip()
@@ -24,8 +24,9 @@ class get_in(threading.Thread):
 
 class Convert480 (threading.Thread):
 	"""Threads to convert video into 480p"""
-	def __init__(self,input_queue):
+	def __init__(self,input_queue,endqueue):
 		threading.Thread.__init__(self)
+		self.endqueue = endqueue
 		self.input_queue = input_queue
 		self.count = 0
 	def run(self):
@@ -34,11 +35,12 @@ class Convert480 (threading.Thread):
 			if not self.input_queue.empty():
 				Input = self.input_queue.get()
 				if Input == 'exit':
+					self.endqueue.put("Ex")
 					break
 				else:
 					file_path = Input
 					self.count += 1
-					command="ffmpeg -i  "+file_path+" -b 1M -r 30 -f mp4 -s 640x480 -loglevel quiet "+file_path[:-4]+"480p"+str(self.count)+".mp4"
+					command="ffmpeg -i  "+file_path+" -y -b 1M -r 30 -f mp4 -s 640x480 -loglevel quiet "+file_path[:-4]+"480p"+".mp4"
 					os.system(command)
 					print(file_path + 'to 480p Finished.\n'+str(self.count)+" Done."+str(self.input_queue.qsize()-1) +" Left.")
 					
@@ -51,9 +53,10 @@ class Convert480 (threading.Thread):
 
 class Convert720 (threading.Thread):
 	"""Threads to convert video into 720p"""
-	def __init__(self,input_queue):
+	def __init__(self,input_queue,endqueue):
 		threading.Thread.__init__(self)
 		self.input_queue = input_queue
+		self.endqueue = endqueue
 		self.count = 0
 	def run(self):
 		print("720 Thread Start")
@@ -61,11 +64,12 @@ class Convert720 (threading.Thread):
 			if not self.input_queue.empty():
 				Input = self.input_queue.get()
 				if Input == 'exit':
+					self.endqueue.put("Ex")
 					break
 				else:
 					file_path = Input
 					self.count += 1
-					command="ffmpeg -i  "+file_path+" -b 2M -r 30 -f mp4 -s 1280x720 -loglevel quiet "+file_path[:-4]+"720p"+str(self.count)+".mp4"
+					command="ffmpeg -i  "+file_path+" -y -b 2M -r 30 -f mp4 -s 1280x720 -loglevel quiet "+file_path[:-4]+"720p"+".mp4"
 					os.system(command)
 					print(file_path + 'to 720p Finished.\n'+str(self.count)+" Done."+str(self.input_queue.qsize()-1) +" Left.")
 			else:
--- main.py
+++ main.py
@@ -1,18 +1,26 @@
 import Threads
 import queue
-def main():
+import time
+def convert():
 	que480 = queue.Queue()
 	que720 = queue.Queue()
+	endqueue1 = queue.Queue()
+	endqueue2 = queue.Queue()
 
 	in_thread = Threads.get_in(que480,que720)
-	conv_720 = Threads.Convert720(que720)
-	conv_480 = Threads.Convert480(que480)
+	conv_720 = Threads.Convert720(que720,endqueue1)
+	conv_480 = Threads.Convert480(que480,endqueue2)
 	conv_720.start()
 	conv_480.start()
 	in_thread.start()
+	while True:
+		if (not endqueue1.empty()) and (not endqueue2.empty()):
+			print('All Done')
+			return 0
+		time.sleep(1)
 
 
 	
 
 if __name__ == '__main__':
-	main()
+	convert()
